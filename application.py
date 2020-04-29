dc2f79f58d4d86b0580e76e0c2e12bd55400a2a5
Teoh Xu En <teoh.xu.en@gmail.com>
2020-04-30 01:20:32 +0800

    Initial changes to application.py

---
+++ application.py
@@ -0,0 +1,12 @@
+from flask import Flask, redirect, render_template, request, session
+from flask_session import Session
+# mkdtemp stores session in filesystem (?)
+from tempfile import mkdtemp
+from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
+from werkzeug.security import check_password_hash, generate_password_hash
+
+# Configure application
+app = Flask(__name__)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
